5052538870879448ab16326e434426996db6996b
Chris Massie <DJ-Massie@Chriss-MacBook-Pro.local>
2017-05-08 10:30:09 -0400

    checkpoint 21 Seek: completed

--- scripts/album.js
+++ scripts/album.js
@@ -18,12 +18,18 @@ var setSong = function(songNumber)
      setVolume(currentVolume);
   };
 
+  var seek = function(time) {
+     if (currentSoundFile) {
+         currentSoundFile.setTime(time);
+     }
+ }
+
   var setVolume = function(volume) {
        if (currentSoundFile) {
            currentSoundFile.setVolume(volume);
        }
    };
-   
+
   var getSongNumberCell = function(number) {
     return $('.song-item-number[data-song-number="' + number + '"]');
   };
@@ -109,6 +115,64 @@ var setCurrentAlbum = function(album) {
         }
     };
 
+var updateSeekBarWhileSongPlays = function() {
+     if (currentSoundFile) {
+         // #10
+         currentSoundFile.bind('timeupdate', function(event) {
+             // #11
+             var seekBarFillRatio = this.getTime() / this.getDuration();
+             var $seekBar = $('.seek-control .seek-bar');
+
+             updateSeekPercentage($seekBar, seekBarFillRatio);
+         });
+     }
+ };
+
+var updateSeekPercentage = function($seekBar, seekBarFillRatio) {
+        var offsetXPercent = seekBarFillRatio * 100;
+        // #1
+        offsetXPercent = Math.max(0, offsetXPercent);
+        offsetXPercent = Math.min(100, offsetXPercent);
+
+        // #2
+        var percentageString = offsetXPercent + '%';
+        $seekBar.find('.fill').width(percentageString);
+        $seekBar.find('.thumb').css({left: percentageString});
+     };
+
+     var setupSeekBars = function() {
+          var $seekBars = $('.player-bar .seek-bar');
+
+          $seekBars.click(function(event) {
+              // #3
+              var offsetX = event.pageX - $(this).offset().left;
+              var barWidth = $(this).width();
+              // #4
+              var seekBarFillRatio = offsetX / barWidth;
+
+              // #5
+              updateSeekPercentage($(this), seekBarFillRatio);
+          });
+
+          $seekBars.find('.thumb').mousedown(function(event) {
+         // #8
+         var $seekBar = $(this).parent();
+
+         // #9
+         $(document).bind('mousemove.thumb', function(event){
+             var offsetX = event.pageX - $seekBar.offset().left;
+             var barWidth = $seekBar.width();
+             var seekBarFillRatio = offsetX / barWidth;
+
+             updateSeekPercentage($seekBar, seekBarFillRatio);
+         });
+         $(document).bind('mouseup.thumb', function() {
+             $(document).unbind('mousemove.thumb');
+             $(document).unbind('mouseup.thumb');
+         });
+       });
+      };
+
 var trackIndex = function(album, song) {
     return album.songs.indexOf(song);
  };
@@ -152,10 +216,11 @@ var currentSoundFile = null;
 var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
- var $nextButton = $('.main-controls .next');
+var $nextButton = $('.main-controls .next');
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
+    setupSeekBars();
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
 });
